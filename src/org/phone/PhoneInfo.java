c575520ea9e4223b4df8610321c114af9bd7505b
Hashimgithub31 <hashim.fshf31@gmail.com>
2021-10-21 20:13:05 +0530

    my task

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,35 @@
+package org.phone;
+
+public class PhoneInfo {
+	
+	private void phoneName() {
+		System.out.println("phone name is Redmi");
+	}
+	
+	private void phoneImeinum() {
+		System.out.println("phone Imei num is 36523565323");
+	}
+	
+	private void camera() {
+		System.out.println("camera is 48mp");
+	}
+	
+	private void storage() {
+		System.out.println("storage capacity is 256gb");
+	
+	}
+	
+	private void osName() {
+		System.out.println("os type is lollipop");
+	}
+	
+	public static void main(String[] args) {
+		PhoneInfo p=new PhoneInfo();
+		p.phoneName();
+		p.phoneImeinum();
+		p.camera();
+		p.storage();
+		p.osName();
+	}
+
+}
